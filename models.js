ebf9dac1b1f9236897b457bb082710f3dc52b1eb
Sokol-IV <missirida@gmail.com>
2020-08-14 22:29:54 +0300

    devicetv class was started to implement

--- models.js
+++ models.js
@@ -42,7 +42,7 @@ class DeviceLamp {
         console.dir(this);
     }
 
-     decreaseBrightness() {
+    decreaseBrightness() {
         if (this.currentBrightness <= this.settings.minBrightness) {
              return;
          }
@@ -73,20 +73,98 @@ class DeviceLamp {
 
 }
 
-const lamp = new DeviceLamp('name', 'type');
 
-console.dir(lamp); 
+class DeviceTv {
+    constructor(name, id, room) {
+        this.name = name;
+        this.type = 'tv';
+        this.id = id;
+        this.isSwitchedOn: false;
+        this.state = {
+            volume: {
+                min: 0,
+                max: 100,
+                current: 0
+            },
+            channel: {
+                min: 1,
+                max: 110,
+                current: 1
+            }
+        };
+        this.room = room || 'some room';
+        console.dir(this);
+    }
 
-lamp.setMaxBrightness();
-console.dir(lamp.currentBrightness);
-lamp.increaseBrightness()
-console.dir(lamp.currentBrightness);
-lamp.decreaseBrightness();
-console.dir(lamp.currentBrightness);
-lamp.increaseBrightness()
-console.dir(lamp.currentBrightness);
-lamp.increaseBrightness()
-console.dir(lamp.currentBrightness);
+    switchOn() {
+        if (this.isSwitchedOn === true) {
+            return;
+        }
+        this.isSwitchedOn = true;
+        console.dir(this);
+     }
+
+     switchOff() {
+        if (this.isSwitchedOn === false) {
+            return;
+        }
+        this.isSwitchedOn = false;
+        console.dir(this);
+    }
+
+    setMin(settingName) {
+        this.state[settingName].current = this.state[settingName].min;
+    }
 
 
+    setMax(settingName) {
+        this.state[settingName].current = this.state[settingName].max;
+    }
 
+    decrease(settingName) {
+        if (this.state[settingName].current <= this.state[settingName].min) {
+             return;
+         }
+        this.state[settingName].current -= 1;
+    }
+
+    increase(settingName) {
+        if (this.state[settingName].current >= this.state[settingName].max) {
+             return;
+         }
+        this.state[settingName].current += 1;
+    }
+
+    setMinVolume() {
+        this.setMin('volume');
+    }
+
+    setMinChannel() {
+        this.setMin('channel');
+    }
+
+    setMaxVolume() {
+        this.setMax('volume');
+    }
+
+    setMaxChannel() {
+        this.setMax('channel');
+    }
+
+    decreaseVolume() {
+        this.decrease('volume');
+    }
+
+    decreaseChannel() {
+        this.decrease('channel');
+    }
+
+    increaseVolume() {
+        this.increase('volume');
+    }
+
+    increaseChannel() {
+        this.increase('channel');
+    }
+
+}
